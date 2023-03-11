9e19b5bf98c8bfed4c66fc82e9717c90c2a8b3f7
Beluga_samsung <2x3tech@gmail.com>
2023-03-10 20:16:22 -0800

    initial commit

---
+++ src/main/java/com/taco/taco_cloud/Controllers/OrderController.java
@@ -0,0 +1,27 @@
+package com.taco.taco_cloud.Controllers;
+
+import com.taco.taco_cloud.Order;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping
+public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm(Model model) {
+        model.addAttribute("order", new Order());
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(Order order) {
+        log.info("Order submitted: " + order);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/taco/taco_cloud/Taco.java
@@ -0,0 +1,16 @@
+package com.taco.taco_cloud;
+
+import lombok.Data;
+
+import java.util.List;
+
+
+@Data
+public class Taco {
+
+
+    private String name;
+    private List<String> ingredients;
+
+}
+
